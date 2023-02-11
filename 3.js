b390351ee6c44dacd07e886091794fd72921ae29
Artem Goncharenko <goncharenko.art@gmail.com>
2023-02-10 21:22:56 -0500

    better solution

--- 3.js
+++ 3.js
@@ -6,14 +6,14 @@
 //
 // Courtesy of projecteuler.net (Problem 1)
 
-function solution(number){
-    let result = [];
-    for (let i=0; i < number; i++) {
-        if ( i % 3 === 0 || i % 5 === 0 || i % 15 === 0) {
-            result.push(i);
+function solution(number) {
+    let sum = 0;
+    for (let i = 0; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
         }
     }
-    return result.reduce((a, b) => a + b, 0);
+    return sum;
 }
 
 console.log(solution(10));
