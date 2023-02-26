f6ceca0e9f8de2b0d9ce4ec3b6c739b4e964d0d0
Artem Goncharenko <goncharenko.art@gmail.com>
2023-02-26 00:12:37 -0500

    next Kata ⚔

--- 23.js
+++ 23.js
@@ -4,11 +4,13 @@
 //
 
 
-class Cuboid {
+class Kata {
     static getVolumeOfCuboid(length, width, height) {
         return length * width * height
     }
 }
+
+console.log(Kata.getVolumeOfCuboid(2,2,2))
 
 // function getVolumeOfCuboid(length, width, height) {
 //     return length * width * height
