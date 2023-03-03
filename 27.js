e3cfc3d717102a14a0aea20d7463f40b96ffd429
Artem Goncharenko <goncharenko.art@gmail.com>
2023-03-02 22:44:15 -0500

    next Kata ⚔

---
+++ 27.js
@@ -0,0 +1,12 @@
+// Complete the function that takes a non-negative integer n as input,
+//     and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive )
+
+function powersOfTwo(n){
+    let array = []
+    for (let i = 0; i <= n; i++) {
+        array.push(2 ** i)
+    }
+    return array
+}
+
+console.log(powersOfTwo(4))
