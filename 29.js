1fb9f89308a92136e40803458c2f3b715abef353
Artem Goncharenko <goncharenko.art@gmail.com>
2023-03-05 22:52:44 -0500

    next Kata ⚔

---
+++ 29.js
@@ -0,0 +1,12 @@
+// Task
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, ' +
+// 'else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+function apple(x){
+    if (x * x > 1000) {
+        return "It's hotter than the sun!!"
+    } else
+        return "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+
+console.log(apple(32))
