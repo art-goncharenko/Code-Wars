ca16e80b5a109483b8587209a141cba8d5e2e9a9
Artem Goncharenko <goncharenko.art@gmail.com>
2023-03-10 23:57:31 -0500

    next Kata ⚔

---
+++ 34.js
@@ -0,0 +1,13 @@
+// Given an array of integers, return a new array with each value doubled.
+//
+//     For example:[1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    let newX = []
+    for (let i = 0; i < x.length; i++) {
+        newX.push(x[i] * 2)
+    }
+    return newX
+}
+
+console.log(maps([1,2,4]))
