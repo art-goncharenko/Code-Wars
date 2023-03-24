a19fd76f86d71831839e0dd8d93f49efefd87205
Artem Goncharenko <goncharenko.art@gmail.com>
2023-03-23 22:08:42 -0400

    next Kata ⚔

---
+++ 45.js
@@ -0,0 +1,15 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+//     String will never be empty and you do not need to account for different data types.
+
+function findShort(s) {
+    let array = s.split(" ");
+    let shortest = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length < shortest.length) {
+            shortest = array[i]
+        }
+    }
+    return shortest.length
+}
+
+console.log(findShort("we know you s are here"))
