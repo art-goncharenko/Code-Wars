33e4dcfa7abeafe8ccb85d1ae153bdd6effb8000
Artem Goncharenko <goncharenko.art@gmail.com>
2023-04-20 23:03:59 -0400

    next kata ⚔

--- 68.js
+++ 68.js
@@ -6,14 +6,14 @@ const rps = (p1, p2) => {
     if (p1 === p2) {
         return "Draw!";
     } else if (
-        (p1 === "Rock" && p2 === "Scissors") ||
-        (p1 === "Scissors" && p2 === "Paper") ||
-        (p1 === "Paper" && p2 === "Rock")
+        (p1 === "rock" && p2 === "scissors") ||
+        (p1 === "scissors" && p2 === "paper") ||
+        (p1 === "paper" && p2 === "rock")
     ) {
-        return "Player 1 wins!";
+        return "Player 1 won!";
     } else {
-        return "Player 2 wins!";
+        return "Player 2 won!";
     }
 };
 
-console.log(rps("Paper", "Rock"))
+console.log(rps("paper", "rock"))
