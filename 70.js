de5e0e28439acb4661e6f828443e440ee1e8e9ea
Artem Goncharenko <goncharenko.art@gmail.com>
2023-04-25 22:57:00 -0400

    next kata ⚔

---
+++ 70.js
@@ -0,0 +1,11 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+function removeUrlAnchor(url) {
+    const index = url.indexOf("#");
+    if (index !== -1) {
+        return url.slice(0, index);
+    }
+    return url;
+}
+
+console.log(removeUrlAnchor("www.ggole.com#2324")); // www.ggole.com
